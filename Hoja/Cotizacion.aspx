a02095a90971c928e2751dbee9d3b661e1c244a2
ecastañeda <ecastaeda@GTDKMARRIOLA>
2016-11-09 17:21:25 -0600

    Add project files.

---
+++ Hoja/Cotizacion.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cotizacion.aspx.cs" Inherits="Hoja.Cotizacion" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+<style>
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #5268B4;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+li a:hover {
+    background-color: #254DDD;
+}
+</style>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <asp:Literal id="litMarkup" runat="server" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ Hoja/Cotizacion.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hoja
+{
+    public partial class Cotizacion : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> locations = new List<string>(); // however this initialized 
+            locations.Add("Home");
+            locations.Add("Cotizador");
+            locations.Add("Panel 1");
+            locations.Add("Panel 2");
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("<ul id=\"tabs\">");
+
+            for (int i = 0; i < locations.Count; i++)
+            {
+                sb.Append("<li id=\"tab" + i.ToString() + "\"><a href=\"" + locations[i] + "\">" + locations[i].ToString() + "</a></li>");
+            }
+
+            sb.Append("</ul>");
+
+            litMarkup.Text = sb.ToString();
+        }
+    }
+}
